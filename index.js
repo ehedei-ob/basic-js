4a090c4861e7cab68f43916278bb4cbd54d36288
Ehedei Hernandez <ehernandez@eturia.es>
2023-01-12 11:27:19 +0000

    Sets

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log('Hello world');
+const set = new Set(['Jesus', 'María', 'José', 'Ehedei']);
+console.log('Set original:', set);
+set.add('Ehedei');
+console.log("Set tras añadir de nuevo 'Ehedei':", set);
+set.add('JavaScript');
+console.log('Set tras añadir JavaScript a la familia', set);
