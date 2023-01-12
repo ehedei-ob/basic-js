56ca89db4c353450b7668ba67c039788ddf9efe0
Ehedei Hernandez <ehernandez@eturia.es>
2023-01-12 10:12:44 +0000

    Topic2

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello world');
+const name = 'Ehedei';
+const age = 39;
+const isDev = true;
+const birthdate = new Date('1983-06-21');
+const favouriteBook = {
+  title: 'Dune',
+  author: 'Frank Herbert',
+  release: new Date('1968-08-12'),
+  url: new URL('https://en.wikipedia.org/wiki/Dune_(novel)')
+};
+
+const list = [name, age, isDev, birthdate, favouriteBook];
+
+console.log(list);
