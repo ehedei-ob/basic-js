44075abb0a4e892fce995fd266a53c874df4b81b
Ehedei Hernandez <ehernandez@eturia.es>
2023-01-12 11:35:28 +0000

    Objects

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('Hello world');
+const yo = {
+  nombre: 'Ehedei',
+  apellido: 'Hernandez',
+  edad: 39,
+  altura: 173,
+  eresDesarrollador: true
+};
+
+console.log('Yo:', yo);
+
+const { edad } = yo;
+console.log('Mi edad', edad);
+
+const amigos = [
+  yo,
+  {
+    nombre: 'Matias',
+    apellido: 'Fernandez',
+    edad: 43,
+    altura: 180,
+    eresDesarrollador: true
+  },
+  {
+    nombre: 'Marcos',
+    apellido: 'Suárez',
+    edad: 42,
+    altura: 179,
+    eresDesarrollador: false
+  }
+];
+
+console.log('Amigos', amigos);
+
+const amigosOrdenados = [...amigos].sort((a, b) => b.edad - a.edad);
+console.log('Amigos ordenados', amigosOrdenados);
