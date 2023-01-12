9d2ea82192827f453bb810bb6a7face7064d8aed
Ehedei Hernandez <ehernandez@eturia.es>
2023-01-12 20:14:16 +0000

    Functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello world');
+function alwaysTrue() {
+  return true;
+}
+
+function saySomethingAfter5Seconds() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Hola soy una promesa');
+      resolve();
+    }, 5000);
+  });
+}
+
+function* ids() {
+  let id = 0;
+  while (true) {
+    yield (id += 2);
+  }
+}
+
+console.log(alwaysTrue());
+saySomethingAfter5Seconds().then(() => {
+  console.log('saySomethingAfter5Seconds resuelto');
+});
+console.log(alwaysTrue());
