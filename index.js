2046986e10f76cd847fdcd2f2f1d93f9fb402c71
Ehedei Hernandez <ehernandez@eturia.es>
2023-01-12 11:49:27 +0000

    Prompt

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Hello world');
+const age = Number(prompt('¿Cual es tu edad?'));
+const message = isNaN(age)
+  ? 'Ha insertado una edad incorrecta'
+  : 'Su edad es: ' + age;
+console.log(message);
