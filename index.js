78a5ca12f1df9221e52f2f7b8352db2662da1605
Ehedei Hernandez <ehernandez@eturia.es>
2023-01-12 11:42:13 +0000

    Dates

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello world');
+const today = new Date();
+const birthdate = new Date('1983-06-21');
+let isTodayAfterBirthdate = today > birthdate;
+let day = birthdate.getDate();
+let month = birthdate.getMonth();
+let year = birthdate.getFullYear();
+
+console.log('today:', today);
+console.log('birthdate:', birthdate);
+console.log('isTodayAfterBirthdate', isTodayAfterBirthdate);
+console.log('day:', day);
+console.log('month:', month);
+console.log('year:', year);
