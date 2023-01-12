bfd3f3710e91525df3625f39b74977aa1f2ba5d0
Ehedei Hernandez <ehernandez@eturia.es>
2023-01-12 10:55:00 +0000

    Topic 4

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello world');
+let firstName = 'Ehedei';
+let name = 'Hernández';
+let student = `${firstName} ${name}`;
+let upperStudent = student.toUpperCase();
+let lowerStudent = student.toLowerCase();
+let lengthStudent = student.length;
+let firstNameFirstChar = firstName[0];
+let nameLastChar = name.slice(-1);
+let studentWithoutSpaces = student.replace(/ /g, '');
+let isNamePresent = student.includes(firstName);
+
+console.log('firstName =', firstName);
+console.log('name =', name);
+console.log('student =', student);
+console.log('upperStudent =', upperStudent);
+console.log('lowerStudent =', lowerStudent);
+console.log('lengthStudent =', lengthStudent);
+console.log('firstNameFirstChar =', firstNameFirstChar);
+console.log('nameLastChar =', nameLastChar);
+console.log('studentWithoutSpaces =', studentWithoutSpaces);
+console.log('isNamePresent =', isNamePresent);
