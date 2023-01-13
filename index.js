58555b2e27ee77261006cb2933ca9351bc07fc9f
Ehedei Hernandez <ehernandez@eturia.es>
2023-01-13 11:40:58 +0000

    OOP

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello world');
+class Estudiante {
+  asignaturas = ['JavaScript', 'HTML', 'CSS'];
+
+  constructor(nombre = 'Nameless') {
+    this.nombre = nombre;
+  }
+
+  obtenDatos() {
+    return {
+      nombre: this.nombre,
+      asignaturas: [...this.asignaturas]
+    };
+  }
+}
+
+const estudiante = new Estudiante();
+console.log(estudiante.obtenDatos());
