79e052aecaf6e21c0b766e43d68a4ea2f1ad0a34
Ehedei Hernandez <ehernandez@eturia.es>
2023-01-13 11:09:27 +0000

    Modules

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello world');
+import { suma, multiplica } from './controllers/controllers.js';
+import chalk from 'chalk';
+
+let op1 = 1;
+let op2 = 2;
+console.log(`Suma ${op1} y ${op2}:`, suma(op1, op2));
+console.log(`Multiplicación ${op1} y ${op2}:`, multiplica(op1, op2));
+
+op1 = 4;
+op2 = 5;
+console.log(`Suma ${op1} y ${op2}:`, suma(op1, op2));
+console.log(
+  chalk.green(`Multiplicación ${op1} y ${op2}:`, multiplica(op1, op2))
+);
