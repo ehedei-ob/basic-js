e6bc457d01394d1580d859931f2547968e226420
Ehedei Hernandez <ehernandez@eturia.es>
2023-01-13 16:02:15 +0000

    Debug

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello world');
+function getFibonacci(limit = 5) {
+  if (limit < 1) {
+    throw Error('Operación no soportada');
+  }
+
+  if (limit === 1) {
+    return [1];
+  }
+
+  const result = [1, 1];
+  for (let i = 2; i < limit; i++) {
+    result.push(result[i - 1] + result[i - 2]);
+  }
+
+  return result;
+}
+
+console.log(getFibonacci(6));
