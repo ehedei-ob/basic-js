150b938b3db7454ce85cb96c152895c5baaaa29f
Ehedei Hernandez <ehernandez@eturia.es>
2023-01-15 10:15:04 +0000

    Storage

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello world');
+const firstName = 'Ehedei';
+const lastName = 'Hernandez';
+
+const person = { firstName, lastName };
+
+const personInJson = JSON.stringify(person);
+
+localStorage.setItem('person', personInJson);
+sessionStorage.setItem('person', personInJson);
+
+const date = new Date();
+date.setMinutes(date.getMinutes() + 2);
+
+document.cookie = `data=${personInJson}; expires=${date}`;
